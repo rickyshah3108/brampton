73bc1e1f74776b1a2e18e5117213a413b9a7b4d2
Ricks <rickyshah3108@gmail.com>
2017-11-07 09:46:48 +0000

    Updated for test

---
+++ 1Darrays.py
@@ -0,0 +1,10 @@
+#1D ARRAY
+#7/7/2017
+
+
+#PSEUDOCODE
+#DECLARE ages: ARRAY[0..9] OF INTEGER
+ages = [3, 4, 5, 3, 5, 6, 7, 3, 1, 6, 7]#this creates the line code#
+for index in range(0 , 10):
+    print(ages[index])
+
---
+++ Inputtingvariablesandprinting.py
@@ -0,0 +1,14 @@
+# declaring details
+#declare firstName : string
+#declare surname : string
+#declare age : integer
+#declare PT : integer
+
+firstName = input("Enter your first name")
+surname = input("Enter your first name")
+age = int(input("Enter your age"))
+PT = input("Enter your PT")
+print(firtname, surname)
+print("Age: ", age)
+print("PT is :", PT)
+ 
